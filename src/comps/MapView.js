da8219b7fb0c85395d87a928ce6d75e9a1a46091
winchester-cell <manili.mm91@gmail.com>
2025-05-17 17:14:02 +0330

    map api key

--- src/comps/MapView.js
+++ src/comps/MapView.js
@@ -38,7 +38,7 @@ export default function MapView() {
 
         // L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png').addTo(map);
 
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png').addTo(map);
+        L.tileLayer(`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.apiKey}`).addTo(map);
 
 
         // Add markers for all devices
